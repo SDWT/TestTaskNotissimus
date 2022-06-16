db0d1adbee9af4b00df59a5324a830947b2ccb7a
Дмитрий Самсонов <NomekopSD@yandex.ru>
2022-06-16 13:51:17 +0300

    Добавьте файлы проекта.

---
+++ TestTaskNotissimus/Entities/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskNotissimus.Entities
+{
+    public class Product
+    {
+        /// <summary>
+        /// Название региона
+        /// </summary>
+        public string RegionName { get; set; }
+
+        /// <summary>
+        /// Хлебные крошки
+        /// </summary>
+        public string BreadCrumbs { get; set; }
+
+        /// <summary>
+        /// Название товара
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Цена старая;
+        /// </summary>
+        public decimal OldPrice { get; set; }
+
+        /// <summary>
+        /// Раздел с наличием(В наличии товар или не в наличии)
+        /// </summary>
+        public bool IsInStock { get; set; }
+
+        /// <summary>
+        /// Ссылки на картинки
+        /// </summary>
+        public string[] ImageUrls { get; set; }
+
+        /// <summary>
+        /// Ссылка на товар
+        /// </summary>
+        public string ProductUrl { get; set; }
+
+    }
+}
---
+++ TestTaskNotissimus/Parsers/ToyRuParser.cs
@@ -0,0 +1,30 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskNotissimus.Entities;
+
+namespace TestTaskNotissimus.Parsers
+{
+    public class ToyRuParser
+    {
+        private string CatalogURL = "https://www.toy.ru/catalog/boy_transport/";
+
+        //public async Task<Product> GetProduct(string address)
+
+        /// <summary>
+        /// Получение данных о товаре
+        /// </summary>
+        /// <param name="address">Ссылка на товар</param>
+        /// <returns>Товар</returns>
+        public async Task GetProduct(string address)
+        {
+            using var context = BrowsingContext.New(Configuration.Default);
+            using var doc = await context.OpenAsync(address);
+
+
+        }
+    }
+}
